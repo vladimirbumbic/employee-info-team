1368912328de1b1b54f7fd1c9f76e6167b62ccbc
Stefan Meza <stefanmeza93@gmail.com>
2022-03-17 12:11:51 +0100

    Add Welcome page for new user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import UserPage from './pages/UserPage';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <h1>Test</h1>
+            <Routes>
+                {/* Ruta za obicnog usera */}
+                <Route path="/user" element={<UserPage />} />
+
+                {/* <Route path="/admin" element={<AdminPage />} />
+                <Route path="/projectm" element={<PMPage />} /> */}
+            </Routes>
         </div>
     );
 }
---
+++ src/components/WelcomeUser/index.jsx
@@ -0,0 +1,11 @@
+import styles from './WelcomeUser.module.css';
+
+const WelcomeUser = () => {
+    return (
+        <main>
+            <h2>Welcome New User</h2>
+        </main>
+    );
+};
+
+export default WelcomeUser;
