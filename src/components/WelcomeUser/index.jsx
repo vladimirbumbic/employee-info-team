e8f2313ffc45c471b9ea9945bc4e95507ac30e4d
Stefan Meza <stefanmeza93@gmail.com>
2022-03-17 12:58:47 +0100

    Add avatar logo for welcome page

--- src/components/WelcomeUser/index.jsx
+++ src/components/WelcomeUser/index.jsx
@@ -1,9 +1,16 @@
+import AvatarLogo from '../../assets/avatarIcon.png';
 import styles from './WelcomeUser.module.css';
 
 const WelcomeUser = () => {
     return (
         <main>
-            <h2>Welcome New User</h2>
+            <div className={styles.card}>
+                <div className={styles.avatarWrapper}>
+                    <img src={AvatarLogo} alt="Avatar Icon"></img>
+                </div>
+                <h2>Welcome User</h2>
+                <p>Frontend Developer</p>
+            </div>
         </main>
     );
 };
