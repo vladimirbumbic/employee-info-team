eaf2c3474a07823de5cf4af8c0aa69f39386642f
Valentina Milićević <62884162+vanjamilicevic@users.noreply.github.com>
2022-03-22 15:03:23 +0100

    Eit 5 (#9)
    * Added public assets
    
    * Finished SideBar and Countries components
    Reorganized folder structure
    
    * Added changes
    
    * Fixed side bar and implemented pending status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import UserPage from './pages/UserPage';
 import AdminPmPage from './pages/AdminPMPage';
+import AdminPage from './pages/AdminPage';
+import LoginPage from './pages/LoginPage';
 import './App.css';
 
 function App() {
@@ -8,9 +10,10 @@ function App() {
     <div className="App">
       <Routes>
         {/* Ruta za obicnog usera */}
-        <Route path="/user" element={<UserPage />} />
+        {/* <Route path="/user" element={<UserPage />} />
 
-        <Route path="/adminpm" element={<AdminPmPage />} />
+        <Route path="/adminpm" element={<AdminPmPage />} /> */}
+        <Route path="/" element={<LoginPage />} />
         {/*   <Route path="/projectm" element={<PMPage />} /> */}
       </Routes>
     </div>
--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,66 +1,67 @@
 import styles from './SideBar.module.css';
 import SideBarItem from '../SideBarItem/Index';
 import { MdOutlineLocationCity, MdManageAccounts } from 'react-icons/md';
-import { FaMapMarkedAlt } from 'react-icons/fa';
+import { FaMapMarkedAlt, FaUserClock } from 'react-icons/fa';
 import { GiLaurelCrown } from 'react-icons/gi';
 import { BsPuzzleFill, BsFillPeopleFill } from 'react-icons/bs';
 import { AiOutlineFundProjectionScreen } from 'react-icons/ai';
 import { RiAdminLine } from 'react-icons/ri';
 
 const SideBar = () => {
-    const options = [
-        {
-            title: 'Countries',
-            icon: <FaMapMarkedAlt />,
-            link: '/pending',
-        },
-        {
-            title: 'Cities',
-            icon: <MdOutlineLocationCity />,
-            link: '/team',
-        },
-        {
-            title: 'Seniorities',
-            icon: <GiLaurelCrown />,
-            link: '/questions',
-        },
-        {
-            title: 'Positions',
-            icon: <BsPuzzleFill />,
-            link: '/companyInfo',
-        },
-        {
-            title: 'Projects',
-            icon: <AiOutlineFundProjectionScreen />,
-            link: '/myProfile',
-        },
-        {
-            title: 'Employees',
-            icon: <BsFillPeopleFill />,
-            link: '/myProfile',
-        },
-        {
-            title: 'PManagers',
-            icon: <MdManageAccounts />,
-            link: '/myProfile',
-        },
-        {
-            title: 'Admins',
-            icon: <RiAdminLine />,
-            link: '/myProfile',
-        },
-    ];
-    return (
-        <main className={styles.sideBarContainer}>
-            {options.map((option, index) => (
-                <SideBarItem
-                    key={index}
-                    title={option.title}
-                    icon={option.icon}
-                />
-            ))}
-        </main>
-    );
+  const options = [
+    {
+      title: 'Pending',
+      icon: <FaUserClock />,
+      link: '/pending',
+    },
+    {
+      title: 'Countries',
+      icon: <FaMapMarkedAlt />,
+      link: '/countries',
+    },
+    {
+      title: 'Cities',
+      icon: <MdOutlineLocationCity />,
+      link: '/cities',
+    },
+    {
+      title: 'Seniorities',
+      icon: <GiLaurelCrown />,
+      link: '/seniorities',
+    },
+    {
+      title: 'Positions',
+      icon: <BsPuzzleFill />,
+      link: '/positions',
+    },
+    {
+      title: 'Projects',
+      icon: <AiOutlineFundProjectionScreen />,
+      link: '/projects',
+    },
+    {
+      title: 'Employees',
+      icon: <BsFillPeopleFill />,
+      link: '/employees',
+    },
+    {
+      title: 'PManagers',
+      icon: <MdManageAccounts />,
+      link: '/pmanagers',
+    },
+    {
+      title: 'Admins',
+      icon: <RiAdminLine />,
+      link: '/admins',
+    },
+  ];
+  return (
+    <main className={styles.sideBarContainer}>
+      {options.map((option, index) => (
+        <SideBarItem key={index} title={option.title} icon={option.icon} />
+      ))}
+    </main>
+  );
 };
 
 export default SideBar;
