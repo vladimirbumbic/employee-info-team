5f4ca73bde334ff8552cdd5e617849a8a63d9404
vanjamilicevic <62884162+vanjamilicevic@users.noreply.github.com>
2022-03-22 14:36:52 +0100

    Fixed side bar and implemented pending status

--- src/pages/AdminPage/index.jsx
+++ src/pages/AdminPage/index.jsx
@@ -1,16 +1,19 @@
 import SideBar from '../../components/SideBar';
 import Countries from '../../components/Countries';
 import styles from './AdminPage.module.css';
+import Navbar from '../../components/Navbar';
 
 const AdminPage = () => {
-    return (
-        <main className={styles.mainContainer}>
-            <SideBar />
-            <div className={styles.componentContainer}>
-                <Countries />
-            </div>
-        </main>
-    );
+  const role = 'admin';
+  return (
+    <main className={styles.mainContainer}>
+      <Navbar role={role} />
+      <SideBar />
+      <div className={styles.componentContainer}>
+        <Countries />
+      </div>
+    </main>
+  );
 };
 
 export default AdminPage;
