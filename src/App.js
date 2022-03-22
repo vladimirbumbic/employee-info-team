863117480f6a7d361eb51fe7e4f67131fcb5fb08
Stefan Meza <stefanmeza93@gmail.com>
2022-03-22 15:16:22 +0100

    Add route for admin page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
         <Route path="/adminpm" element={<AdminPmPage />} /> */}
         <Route path="/" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminPage />} />
         {/*   <Route path="/projectm" element={<PMPage />} /> */}
       </Routes>
     </div>
