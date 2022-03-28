6a1aa55b66b6964eb3c7328d60080663129a2d3f
vanjamilicevic <62884162+vanjamilicevic@users.noreply.github.com>
2022-03-28 13:19:29 +0200

    Made changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
-import UserPage from './pages/UserPage';
-import AdminPage from './pages/AdminPage';
+
 import LoginPage from './pages/LoginPage';
 import { SidebarProvider } from './contexts/SidebarContext';
+import MainContent from './pages/MainContent';
 import './App.css';
-import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      {/* Ruta za obicnog usera */}
-      {/* <Route path="/user" element={<UserPage />} />}
+    <SidebarProvider>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
 
-        // <Route path="/adminpm" element={<AdminPmPage />} /> */}
-      <Routes>
-        <Route exact path="/" element={<LoginPage />} />
-
-        <Route exact path="/mainContent/*" element={<MainContent />} />
-      </Routes>
-
-      {/*   <Route path="/projectm" element={<PMPage />} /> */}
-    </div>
+          <Route exact path="/mainContent/*" element={<MainContent />} />
+        </Routes>
+      </div>
+    </SidebarProvider>
   );
 }
 
