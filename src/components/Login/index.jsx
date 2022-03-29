ff4e2e5d789f9c55eb3b05d73fa487cca9d45fd1
Stefan Meza <stefanmeza93@gmail.com>
2022-03-29 16:07:07 +0200

    Add axios package

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,8 +1,19 @@
 import styles from './Login.module.css';
 import { NavLink } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const Login = () => {
+  const handleLogin = async () => {
+    try {
+      const response = await axios('localhost:5000/auth/google');
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main className={styles.pageContainer}>
       <main className={styles.mainContainer}>
@@ -16,14 +27,14 @@ const Login = () => {
           </div>
         </div>
         <div className={styles.loginGoogleOauthContainer}>
-          <NavLink to="/mainContent" style={{ textDecoration: 'none' }}>
-            <div className={styles.googleOAuthContainer}>
-              <div className={styles.googleOAuthLogo}></div>
-              <p className={styles.googleOAuthTitle}>
-                Continue with <span className={styles.google}>Google</span>
-              </p>
-            </div>
-          </NavLink>
+          {/* <NavLink to="/mainContent" style={{ textDecoration: 'none' }}> */}
+          <div className={styles.googleOAuthContainer} onClick={handleLogin}>
+            <div className={styles.googleOAuthLogo}></div>
+            <p className={styles.googleOAuthTitle}>
+              Continue with <span className={styles.google}>Google</span>
+            </p>
+          </div>
+          {/* </NavLink> */}
         </div>
       </main>
     </main>
