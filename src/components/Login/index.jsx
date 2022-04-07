5f2b95b156238c80a628d7ce7470ca751b6bc2c7
Tanja <tanja120a@gmail.com>
2022-04-07 14:08:30 +0200

    Log with Google

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,39 +1,75 @@
-import { useContext } from 'react';
-import styles from './Login.module.css';
-import { NavLink } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa';
-import login from '../../api/login';
-import { useApi } from '../../hooks/useApi';
+import {useState} from 'react';
+import classes from './Login.module.css';
+import GoogleLogin from 'react-google-login';
 
-const Login = () => {
-  const { data, request } = useApi(login);
+function App() {
 
+  const [ loginData, setLoginData] = useState(localStorage.getItem('loginData')
+  ? JSON.parse(localStorage.getItem('loginData')): null);
+  
+
+  const handleLoginFailure = (result) => {
+    alert(result);
+  }
+
+  const handleLogin = async (googleData) => {
+    const res = await fetch('api/google-login', {
+      method: 'POST',
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }), 
+      headers: { 
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log(googleData)
+
+    const data = await res.json();
+    console.log(data)
+    setLoginData(data);
+    localStorage.setItem('loginData', JSON.stringify(data));
+    
+    
+  }
+
+  const handleLogOut = () => {
+    localStorage.removeItem('loginData');
+    setLoginData(null)
+  }
+
+  console.log(loginData)
   return (
-    <main className={styles.pageContainer}>
-      <main className={styles.mainContainer}>
-        <div className={styles.loginImageContainer}>
-          <div className={styles.laptopImage}></div>
-          <div className={styles.loginTitleContainer}>
-            <div className={styles.loginTitleIcon}>
-              <FaUserCircle className={styles.userIcon} />
-              <p className={styles.loginTitle}>Login</p>
-              <a href="http://localhost:5000/auth/google">Login</a>
+    <div className={classes.app}>
+      <header className={classes.header}>
+        <div className={classes.fade}>   
+      <div className={classes.title}>     
+      { loginData?<p className = {classes.p}></p>:<p className = {classes.p}>login</p> }
+      </div>
+        <div className = {classes.btn}>
+        {
+          loginData?(
+            <div>
+              <h6 className={classes.logged}>You are now logged in as
+                <span className = {classes.span}> {loginData.email}</span>
+              </h6>
+              <button className={classes.logout} onClick={handleLogOut}>Log out</button>
             </div>
-          </div>
+          ): (
+            <GoogleLogin
+          clientId = {process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText = "Log with Google"
+          onSuccess = {handleLogin}
+          onFailure = {handleLoginFailure}
+          cookiePolicy={'single_host_origin'}
+          
+          ></GoogleLogin>
+          )
+        }  
         </div>
-        <div className={styles.loginGoogleOauthContainer}>
-          {/* <NavLink to="/mainContent" style={{ textDecoration: 'none' }}> */}
-          <div className={styles.googleOAuthContainer} onClick={request}>
-            <div className={styles.googleOAuthLogo}></div>
-            <p className={styles.googleOAuthTitle}>
-              Continue with <span className={styles.google}>Google</span>
-            </p>
-          </div>
-          {/* </NavLink> */}
         </div>
-      </main>
-    </main>
+      </header>
+    </div>
   );
-};
+}
 
-export default Login;
+export default App;
