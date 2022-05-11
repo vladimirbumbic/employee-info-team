75c1a75a571855908c78f1128dabc398e2f1fc23
vladimirbumbic <vladimirbumbic@gmail.com>
2022-05-11 14:04:16 +0200

    conditional card rendering of all employees...

--- src/components/AllEmployees/index.jsx
+++ src/components/AllEmployees/index.jsx
@@ -1,27 +1,30 @@
 import { useState } from 'react';
-
 import EmployeeCard from '../EmployeeCard';
 import styles from './AllEmployees.module.css';
 import AddEmployee from '../AddEmployee';
 import { useContext, useEffect } from 'react';
 import AuthContext from '../../contexts/AuthContext';
 import { getAllEmployees } from '../../services/employees';
-import EmployeeModal from '../EmployeeModal';
 
 const AllEmployees = () => {
   const { users, setUsers } = useContext(AuthContext);
-
+  const [allEmployeesLoaded, setAllEmployeesLoaded] = useState(false);
   console.log(users);
 
   const handleAllEmployees = async () => {
     const allEmployees = await getAllEmployees();
     const employees = allEmployees.data;
     setUsers(employees);
+    setAllEmployeesLoaded(true);
   };
 
   useEffect(() => {
     handleAllEmployees();
   }, []);
+
+  if (!allEmployeesLoaded) {
+    return 'Loading...';
+  }
 
   return (
     <main>
