0d01733b664c071c7a4085b80936b811c141fc9a
vladimirbumbic <vladimirbumbic@gmail.com>
2022-05-17 10:34:36 +0200

    pagination set

--- src/components/Employees/index.jsx
+++ src/components/Employees/index.jsx
@@ -1,10 +1,15 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import EmployeeContext from '../../contexts/EmployeeContext';
 import EmployeeCard from '../EmployeeCard';
 import style from './Employees.module.css';
+import ReactPaginate from 'react-paginate';
 
 const Employees = () => {
   const { getAllUsers, allUsers, navbarSearch } = useContext(EmployeeContext);
+
+  const [pageNumber, setPageNumber] = useState(0);
+  const usersPerPage = 100;
+  const pagesVisited = pageNumber * usersPerPage;
 
   useEffect(() => {
     getAllUsers();
@@ -21,22 +26,48 @@ const Employees = () => {
     );
   }, [navbarSearch]);
 
+  const displayUsers = allUsers
+    .filter((user) => {
+      if (!navbarSearch) {
+        return user;
+      } else if (user.name.toLowerCase().includes(navbarSearch.toLowerCase())) {
+        return user;
+      }
+    })
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return (
+        <EmployeeCard
+          key={user.id}
+          name={user.name}
+          mail={user.email}
+          photo={user.photo}
+          role={user.role}
+          id={user.id}
+          seniority={user.seniority}
+          city={user.city}
+          technology={user.mainTechnology}
+        />
+      );
+    });
+
+  const pageCount = Math.ceil(allUsers.length / usersPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <div className={style.background}>
       <div className={style.employees}>
-        {allUsers
-          .filter((user) => {
-            if (!navbarSearch) {
-              return user;
-            } else if (
-              user.name.toLowerCase().includes(navbarSearch.toLowerCase())
-            ) {
-              return user;
-            }
-          })
-          .map((user) => {
-            return <EmployeeCard key={user.id} {...user} />;
-          })}
+        {displayUsers}
+        <ReactPaginate
+          previousLabel={'Previous'}
+          nextLabel={'Next'}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={style.paginationButtons}
+          activeClassName={style.paginationActive}
+        />
       </div>
     </div>
   );
